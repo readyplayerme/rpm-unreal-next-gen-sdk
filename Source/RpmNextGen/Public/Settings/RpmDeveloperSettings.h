c835dbdf558464a6f69871705b885aa3eecef87c
Harrison <harrisonhough4@gmail.com>
2024-08-22 11:21:24 +0300

    chore: update dev settings

--- Source/RpmNextGen/Public/Settings/RpmDeveloperSettings.h
+++ Source/RpmNextGen/Public/Settings/RpmDeveloperSettings.h
@@ -13,28 +13,35 @@ UCLASS(config = Game, defaultconfig, meta = (DisplayName = "Ready Player Me"))
 class RPMNEXTGEN_API URpmDeveloperSettings : public UDeveloperSettings
 {
 	GENERATED_BODY()
+	
+	UPROPERTY(VisibleAnywhere, Config, Category = "Auth Settings", meta = (ReadOnly  = "true", ToolTip = "Base URL for requests."))
+	FString ApiBaseUrl;
 
 public:
 	URpmDeveloperSettings();
 
-	UPROPERTY(EditAnywhere, Config, Category = "API")
+	UPROPERTY(VisibleAnywhere, Config, Category = "Auth Settings", meta = (ReadOnly  = "true", ToolTip = "Base URL for authentication requests."))
 	FString ApiBaseAuthUrl;
-	
-	UPROPERTY(EditAnywhere, Config, Category = "API")
+
+	UPROPERTY(EditAnywhere, Config, Category = "Auth Settings", meta = (ToolTip = "Application ID used for authentication."))
 	FString ApplicationId;
 
-	UPROPERTY(EditAnywhere, Config, Category = "API")
+	UPROPERTY(EditAnywhere, Config, Category = "Auth Settings", meta = (ToolTip = "API key used for authentication."))
 	FString ApiKey;
 	
-	UPROPERTY(EditAnywhere, Config, Category = "API")
+	UPROPERTY(EditAnywhere, Config, Category = "Auth Settings", meta = (ToolTip = "Proxy URL for API requests. If empty, the base URL will be used."))
 	FString ApiProxyUrl;
 
 	void SetupDemoAccount();
 	void Reset();
 	FString GetApiBaseUrl();
 
+	bool IsValid() const
+	{
+		return !ApplicationId.IsEmpty() && (!ApiKey.IsEmpty() || !ApiProxyUrl.IsEmpty());
+	}
+
 private:	
-	FString ApiBaseUrl;
 	const FString DemoAppId = TEXT("665e05a50c62c921e5a6ab84");
 	const FString DemoProxyUrl = TEXT("https://api.readyplayer.me/demo");
 };
