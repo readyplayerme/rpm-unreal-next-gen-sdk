02b9d00c039b3cd77486fe78d45ab121d21fc584
Harrison Hough <harrisonhough4@gmail.com>
2024-09-13 11:49:16 +0300

    chore: fix filepath

--- Source/RpmNextGen/Public/Api/Files/FileUtility.h
+++ Source/RpmNextGen/Public/Api/Files/FileUtility.h
@@ -9,11 +9,17 @@ class RPMNEXTGEN_API FFileUtility
 public:
 	static bool SaveToFile(const TArray<uint8>& Data, const FString& FilePath, const bool bSkipIfFileExists = true)
 	{
-		if (bSkipIfFileExists && FPaths::FileExists(FilePath) || FFileHelper::SaveArrayToFile(Data, *FilePath))
+		if ((bSkipIfFileExists && FPaths::FileExists(FilePath)))
+		{
+			UE_LOG(LogReadyPlayerMe, Log, TEXT("File already exists at: %s. Skipping"), *FilePath);
+			return true;
+		}
+		if (FFileHelper::SaveArrayToFile(Data, *FilePath))
 		{
 			UE_LOG(LogReadyPlayerMe, Log, TEXT("Successfully saved asset to: %s"), *FilePath);
 			return true;
 		}
+
 		UE_LOG(LogReadyPlayerMe, Error, TEXT("Failed to save asset to: %s"), *FilePath);
 		return false;
 	}
