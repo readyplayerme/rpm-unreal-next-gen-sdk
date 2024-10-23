94f8df39b519311a9c7513e4500196bc6ef6f355
Harrison Hough <harrisonhough4@gmail.com>
2024-10-23 14:50:39 +0300

    chore: update create request data

--- Source/RpmNextGen/Public/Api/Auth/Models/CreateUserRequest.h
+++ Source/RpmNextGen/Public/Api/Auth/Models/CreateUserRequest.h
@@ -4,7 +4,7 @@
 #include "CreateUserRequest.generated.h"
 
 USTRUCT(BlueprintType)
-struct RPMNEXTGEN_API FCreateUserRequest
+struct RPMNEXTGEN_API FCreateUserRequestBody
 {
 	GENERATED_BODY()
 
@@ -13,4 +13,34 @@ struct RPMNEXTGEN_API FCreateUserRequest
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ready Player Me|Auth|Request", meta = (json = "requestToken"))
 	bool RequestToken = true;
+
+	FCreateUserRequestBody()
+	{
+		ApplicationId = TEXT("");
+		RequestToken = true;
+	}
+	FCreateUserRequestBody(const FString& InApplicationId, const bool InRequestToken = true)
+	{
+		ApplicationId = InApplicationId;
+		RequestToken = InRequestToken;
+	}
+};
+
+USTRUCT(BlueprintType)
+struct RPMNEXTGEN_API FCreateUserRequest
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ready Player Me|Auth|Request", meta = (json = "data"))
+	FCreateUserRequestBody Data;
+
+	FCreateUserRequest()
+	{
+		Data = FCreateUserRequestBody();
+	}
+	
+	FCreateUserRequest(const FString& InApplicationId, const bool InRequestToken = true)
+	{
+		Data = FCreateUserRequestBody(InApplicationId, InRequestToken);
+	}
 };
